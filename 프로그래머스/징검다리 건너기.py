f71f8797d87373de13f22fabe9bb7c2ccf74d6d4
혜진 <hyeluceinaltis@naver.com>
2023-09-06 23:54:37 +0900

    0906

---
+++ 프로그래머스/징검다리 건너기.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def solution(stones, k):
+    max_arr = []
+
+    q = deque()
+    q.append((-stones[0], 0))
+    max_arr.append(stones[0])
+
+    for i in range(1, len(stones)):
+        tmp = stones[i]
+
+        while q and -q[-1][0] < tmp:
+            q.pop()
+
+        q.append((-stones[i], i))
+
+        while i - q[0][1] >= k:
+            q.popleft()
+
+        max_arr.append(-q[0][0])
+
+    return min(max_arr[k-1:])
+
+stones = list(map(int, input().split()))
+k = int(input())
+print(solution(stones, k))
